45831d1049806c1cbbcbab3906d7444272c2e5fd
Yihong Wang <yh.wang@ibm.com>
2018-08-22 15:53:32 -0700

    fix: copy public directory to correct location
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

--- tools/copyStatic.js
+++ tools/copyStatic.js
@@ -4,4 +4,4 @@ const shell = require('shelljs');
 const rootDir = path.join(__dirname, '..');
 
 shell.cp('-R', path.join(rootDir, 'src', 'public'),
-  path.join(rootDir, 'dist', 'public'));
+  path.join(rootDir, 'dist'));
