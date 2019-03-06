779e0c89aaedcac9188c5a7ca0eee8d33b08acdc
Paul Van Eck <pvaneck@us.ibm.com>
2019-03-05 17:08:21 -0800

    Update client README

--- client/src/components/AppliedRoute.js
+++ client/src/components/AppliedRoute.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+/**
+ * This handles routes where it doesn't matter if the user is logged in or not.
+ */
 export default ({ component: C, props: cProps, ...rest }) =>
   <Route {...rest} render={props => <C {...props} {...cProps} />} />;
